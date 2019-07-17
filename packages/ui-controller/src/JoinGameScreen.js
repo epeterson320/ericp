7443cf7a56328c6f55c3f6ce331c929e71c00351
Eric Peterson <eric.peterson@cognosante.com>
2019-07-17 16:18:27 -0400

    Add websocket endpoint

--- packages/ui-controller/src/JoinGameScreen.js
+++ packages/ui-controller/src/JoinGameScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import noop from 'lodash/noop';
+import debug from 'debug';
+
+const log = debug('crazytown:ui-controller:JoinGameScreen');
 
 export default function JoinGameScreen({ onGameStarted, onGameJoined }) {
 	const listing = useGameListing();
@@ -9,7 +11,7 @@ export default function JoinGameScreen({ onGameStarted, onGameJoined }) {
 		<div className="App">
 			<h1>Welcome to Crazytown</h1>
 			{loading ? <p>Searching for games...</p> : null}
-			{error ? <pre>{listing.error.message}</pre> : null}
+			{error ? <pre>{error.message}</pre> : null}
 			{error ? <button onClick={refetch}>Retry</button> : null}
 			<button
 				disabled={loading || error}
@@ -34,24 +36,38 @@ export default function JoinGameScreen({ onGameStarted, onGameJoined }) {
 }
 
 function useGameListing() {
-	const [listing, setListing] = React.useState({
-		loading: true,
-		games: [],
-		error: null,
-		refetch: noop,
-	});
+	const [loading, setLoading] = React.useState(true);
+	const [games, setGames] = React.useState([]);
+	const [error, setError] = React.useState(null);
+	const [refetchCounter, setRefetchCounter] = React.useState(0);
+
 	React.useEffect(() => {
-		const timer = setTimeout(() => {
-			setListing({
-				loading: false,
-				games: [{ id: 5 }],
-				error: null,
-				refetch: noop,
-			});
-		}, 2000);
+		const ws = new WebSocket(`ws://${process.env.REACT_APP_WS_HOST}/games`);
+		ws.onopen = () => {
+			setLoading(false);
+		};
+		ws.onerror = event => {
+			setError(new Error('Error connecting to server.'));
+			setLoading(false);
+			log('Websocket Error Event: %O', event);
+		};
+		ws.onmessage = msg => {
+			setGames(JSON.parse(msg));
+		};
 
-		return () => clearTimeout(timer);
-	}, []);
+		return () => {
+			delete ws.onopen;
+			delete ws.onerror;
+			delete ws.onmessage;
+			ws.close(1000);
+		};
+	}, [refetchCounter]);
 
-	return listing;
+	const refetch = () => {
+		setLoading(true);
+		setError(null);
+		setRefetchCounter(refetchCounter + 1);
+	};
+
+	return { loading, games, error, refetch };
 }
